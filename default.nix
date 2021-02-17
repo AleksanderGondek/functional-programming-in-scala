50f8d4ac6339592e64c03764770ab18d604b33bc
Aleksander Gondek <gondekaleksander@protonmail.com>
2021-02-17 17:25:35 +0100

    Update used nix pkgs manager in githubactions

--- default.nix
+++ default.nix
@@ -1,7 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ }:
 
+let
+  pkgs = import (
+    fetchTarball { url = https://github.com/NixOS/nixpkgs/archive/nixos-20.09.tar.gz;}
+  ) {};
+  name = "fp-in-scala-shell";
+in
 pkgs.mkShell {
+  inherit name;
+
   shellHook = ''
+    echo 'Welcome to ${name}!'
   '';
 
   buildInputs = with pkgs; [
