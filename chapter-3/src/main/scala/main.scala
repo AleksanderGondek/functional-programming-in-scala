85360e5b2ed9a0879972d8608106cb569081c7fd
Aleksander Gondek <gondekaleksander@protonmail.com>
2021-03-29 20:23:44 +0200

    3.24 done

--- chapter-3/src/main/scala/main.scala
+++ chapter-3/src/main/scala/main.scala
@@ -1,33 +1,36 @@
-sealed trait List[+A]
-case object Nil extends List[Nothing]
+sealed trait CList[+A]
+case object CNil extends CList[Nothing]
 
-case class Cons[+A](head: A, tail: List[A]) extends List[A]
+case class CCons[+A](head: A, tail: CList[A]) extends CList[A]
 
-object List {
-  def apply[A](as: A*): List[A] =
-    if (as.isEmpty) Nil
-    else Cons(as.head, apply(as.tail: _*))
+// C stands for Custom
+// Custom List, Custom Nil, Custom Cons
+
+object CList {
+  def apply[A](as: A*): CList[A] =
+    if (as.isEmpty) CNil
+    else CCons(as.head, apply(as.tail: _*))
   
   // Exercise 3.2
-  def tail[A](l: List[A]): List[A] = l match {
-    case Nil => Nil
-    case Cons(_, tail) => tail
+  def tail[A](l: CList[A]): CList[A] = l match {
+    case CNil => CNil
+    case CCons(_, tail) => tail
   }
 
   // Exercise 3.3
-  def setHead[A](l: List[A], x: A): List[A] = l match {
-    case Nil => Cons(x, Nil)
-    case Cons(_, t) => Cons(x, t)
+  def setHead[A](l: CList[A], x: A): CList[A] = l match {
+    case CNil => CCons(x, CNil)
+    case CCons(_, t) => CCons(x, t)
   }
  
   // Exercise 3.4
-  def drop[A](l: List[A], n: Int): List[A] = {
+  def drop[A](l: CList[A], n: Int): CList[A] = {
     @annotation.tailrec
-    def loop(lst: List[A], i: Int): List[A] = {
+    def loop(lst: CList[A], i: Int): CList[A] = {
       if (i >= n) lst
       else lst match {
-        case Nil => Nil
-        case Cons(_, tail) => loop(tail, i+1)
+        case CNil => CNil
+        case CCons(_, tail) => loop(tail, i+1)
       }
     }
 
@@ -35,15 +38,15 @@ object List {
   }
 
   // Exercise 3.5
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+  def dropWhile[A](l: CList[A], f: A => Boolean): CList[A] = {
     l match {
-      case Cons(head, tail) if f(head) => dropWhile(tail, f)
+      case CCons(head, tail) if f(head) => dropWhile(tail, f)
       case _ => l
     }
   }
 
   // Exercise 3.6
-  def init[A](l: List[A]): List[A] = {
+  def init[A](l: CList[A]): CList[A] = {
     // Because we are not using scala built-in list
     // and we do not know anything about lazy list / streams
     // however we HAVE TO prevent stack overflow
@@ -51,11 +54,11 @@ object List {
     val buffer = new ListBuffer[A]
 
     @annotation.tailrec
-    def loop(lst: List[A]): List[A] = {
+    def loop(lst: CList[A]): CList[A] = {
       lst match {
-        case Nil => Nil
-        case Cons(_, Nil) => List(buffer.toList: _*)
-        case Cons(head, tail) => {
+        case CNil => CNil
+        case CCons(_, CNil) => CList(buffer.toList: _*)
+        case CCons(head, tail) => {
           buffer.append(head)
           loop(tail)
         }
@@ -64,18 +67,18 @@ object List {
     loop(l)
   }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A,B) => B): B = {
+  def foldRight[A,B](as: CList[A], z: B)(f: (A,B) => B): B = {
     as match {
-      case Nil => z
-      case Cons(head, tail) => f(head, foldRight(tail, z)(f))
+      case CNil => z
+      case CCons(head, tail) => f(head, foldRight(tail, z)(f))
     }
   }
 
-  def sum(ints: List[Int]): Int = {
+  def sum(ints: CList[Int]): Int = {
     foldRight(ints, 0)((x,y) => x + y)
   }
 
-  def product(ds: List[Double]): Double = {
+  def product(ds: CList[Double]): Double = {
     foldRight(ds, 1.0)(_ * _)
   }
 
@@ -84,83 +87,83 @@ object List {
   // so, early halt is not possible
 
   // Exercise 3.9
-  def length[A](as: List[A]): Int = {
+  def length[A](as: CList[A]): Int = {
     // as match {
-    //   case Nil => 0
-    //   case Cons(head, tail) => 1 + length(tail)
+    //   case CNil => 0
+    //   case CCons(head, tail) => 1 + length(tail)
     // }
     foldRight(as, 0)((_,acc) => acc +1)
   }
 
   // Exercise 3.10
   @annotation.tailrec
-  def foldLeft[A,B](l: List[A], z: B)(f: (B,A) => B): B = {
+  def foldLeft[A,B](l: CList[A], z: B)(f: (B,A) => B): B = {
     l match {
-      case Nil => z
-      case Cons(head, tail) => foldLeft(tail, f(z, head))(f)
+      case CNil => z
+      case CCons(head, tail) => foldLeft(tail, f(z, head))(f)
     }
   }
 
   // Exercise 3.11
-  def productLeft(ds: List[Double]): Double = {
+  def productLeft(ds: CList[Double]): Double = {
     foldLeft(ds, 1.0)(_ * _)
   }
 
   // Exercise 3.12
-  def reverse[A](l: List[A]): List[A] = {
-    foldLeft(l, List[A]())(
-      (all, x) => Cons(x, all)
+  def reverse[A](l: CList[A]): CList[A] = {
+    foldLeft(l, CList[A]())(
+      (all, x) => CCons(x, all)
     )
   }
 
   // Exercise 3.13
-  def foldLeftViaFoldRight[A,B](l: List[A], z: B)(f: (B,A) => B): B = {
+  def foldLeftViaFoldRight[A,B](l: CList[A], z: B)(f: (B,A) => B): B = {
     foldRight(l, (b:B) => b)((a,g) => b => g(f(b,a)))(z)
   }    
 
-  def foldRightViaFoldLeft[A,B](l: List[A], z: B)(f: (A,B) => B): B = {
+  def foldRightViaFoldLeft[A,B](l: CList[A], z: B)(f: (A,B) => B): B = {
     foldLeft(reverse(l), z)((b,a) => f(a,b))
   }
 
   // Exercise 3.14
-  def append[A](a1: List[A], a2: List[A]): List[A] = {
+  def append[A](a1: CList[A], a2: CList[A]): CList[A] = {
     foldRightViaFoldLeft(a1, a2)(
-      Cons(_,_)
+      CCons(_,_)
     )
   }
 
   // Exercise 3.15
-  def myLittleFlatten[A](l: List[List[A]]): List[A] = {
-    foldLeft(l, List[A]())(append)
+  def myLittleFlatten[A](l: CList[CList[A]]): CList[A] = {
+    foldLeft(l, CList[A]())(append)
   }
 
   // Exercise 3.16
-  def transformPlusOne(l: List[Int]): List[Int] = {
-    foldRightViaFoldLeft(l, List[Int]())(
-      (head, tail) => Cons(head+1, tail)
+  def transformPlusOne(l: CList[Int]): CList[Int] = {
+    foldRightViaFoldLeft(l, CList[Int]())(
+      (head, tail) => CCons(head+1, tail)
     )
   }
 
   // Exercise 3.17
-  def stringify(l: List[Double]): List[String] = {
-    foldRightViaFoldLeft(l, List[String]())(
-      (head, tail) => Cons(head.toString(), tail)
+  def stringify(l: CList[Double]): CList[String] = {
+    foldRightViaFoldLeft(l, CList[String]())(
+      (head, tail) => CCons(head.toString(), tail)
     )
   }
 
   // Exercise 3.18
-  def map[A,B](as: List[A])(f: A => B): List[B] = {
-    foldRightViaFoldLeft(as, List[B]())(
-      (head, tail) => Cons(f(head), tail)
+  def map[A,B](as: CList[A])(f: A => B): CList[B] = {
+    foldRightViaFoldLeft(as, CList[B]())(
+      (head, tail) => CCons(f(head), tail)
     )
   }
 
   // Exercise 3.19
-  def filter[A](as: List[A])(f: A => Boolean): List[A] = {
-    foldRightViaFoldLeft(as, List[A]())(
+  def filter[A](as: CList[A])(f: A => Boolean): CList[A] = {
+    foldRightViaFoldLeft(as, CList[A]())(
       (head, tail) => {
         if(f(head)) {
-          Cons(head, tail)
+          CCons(head, tail)
         } else {
           tail
         }
@@ -169,117 +172,153 @@ object List {
   }
 
   // Exercise 3.20
-  def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = {
-    List.myLittleFlatten(List.map(as)(f))
+  def flatMap[A,B](as: CList[A])(f: A => CList[B]): CList[B] = {
+    CList.myLittleFlatten(CList.map(as)(f))
   }
 
   // Exercise 3.21
-  def filterViaFlatMap[A](as: List[A])(f: A => Boolean): List[A] = {
-    List.flatMap(as)(x => if (f(x)) List(x) else List())
+  def filterViaFlatMap[A](as: CList[A])(f: A => Boolean): CList[A] = {
+    CList.flatMap(as)(x => if (f(x)) CList(x) else CList())
   }
 
   // Exercise 3.22
-  def addPairs(as: List[Int], at: List[Int]): List[Int] = {
+  def addPairs(as: CList[Int], at: CList[Int]): CList[Int] = {
     (as, at) match {
-      case (_, Nil) => Nil
-      case (Nil, _) => Nil
-      case(Cons(as_head, as_tail), Cons(at_head, at_tail)) => Cons(as_head+at_head, addPairs(as_tail, at_tail))
+      case (_, CNil) => CNil
+      case (CNil, _) => CNil
+      case(CCons(as_head, as_tail), CCons(at_head, at_tail)) => CCons(as_head+at_head, addPairs(as_tail, at_tail))
     }
   }
 
   // Exercise 3.23
-  def zipWith[A](xr: List[A], xs: List[A])(f: (A, A) => A): List[A] = {
+  def zipWith[A](xr: CList[A], xs: CList[A])(f: (A, A) => A): CList[A] = {
     (xr, xs) match {
-      case (_, Nil) => Nil
-      case (Nil, _) => Nil
-      case(Cons(xr_head, xr_tail), Cons(xs_head, xs_tail)) => Cons(f(xr_head, xs_head), zipWith(xr_tail, xs_tail)(f))
+      case (_, CNil) => CNil
+      case (CNil, _) => CNil
+      case(CCons(xr_head, xr_tail), CCons(xs_head, xs_tail)) => CCons(f(xr_head, xs_head), zipWith(xr_tail, xs_tail)(f))
+    }
+  }
+
+  // Exercise 3.24
+  @annotation.tailrec
+  def _begins_with[A](xr: CList[A], xs: CList[A]): Boolean = {
+    (xr, xs) match {
+      case (CNil, CNil) => true
+      // In general, CList does not 'begin with' CNil,
+      // but this implementation allows us to built 
+      // recursive solution.
+      case (_, CNil) => true
+      case (CCons(xr_head, xr_tail), CCons(xs_head, xs_tail)) if (xr_head == xs_head) => _begins_with(xr_tail, xs_tail)
+      case _ => false
+    }
+  }
+
+  @annotation.tailrec
+  def hasSubsequence[A](sup: CList[A], sub: CList[A]): Boolean = {
+    sup match {
+      case CNil => sub == CNil
+      case _ if _begins_with(sup, sub) => true
+      case CCons(_, sup_tails) => hasSubsequence(sup_tails, sub)
     }
   }
 }
 
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("// Exercise 3.1")
-    val x = List(1,2,3,4,5) match {
-      case Cons(x, Cons(2, Cons(4, _))) => x
-      case Nil => 42
-      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-      case Cons(h, t) => h + List.sum(t)
+    val x = CList(1,2,3,4,5) match {
+      case CCons(x, CCons(2, CCons(4, _))) => x
+      case CNil => 42
+      case CCons(x, CCons(y, CCons(3, CCons(4, _)))) => x + y
+      case CCons(h, t) => h + CList.sum(t)
       case _ => 101
     }
 
     println(x) // Prints 3!
 
     println("// Exercise 3.2")
-    val exampleList = List(1,2,3,4,5)
-    println(List.tail(exampleList))
-    println(List())
+    val exampleList = CList(1,2,3,4,5)
+    println(CList.tail(exampleList))
+    println(CList())
 
     println("// Exercise 3.3")
-    println(List.setHead(exampleList, 6))
-    println(List.setHead(List(), 6))
+    println(CList.setHead(exampleList, 6))
+    println(CList.setHead(CList(), 6))
 
     println("// Exercise 3.4")
-    println(List.drop(List(), 0))
-    println(List.drop(List(), 1))
-    println(List.drop(List(1,2,3,4,5), 0))
-    println(List.drop(List(1,2,3,4,5), 1))
-    println(List.drop(List(1,2,3,4,5), 5))
-    println(List.drop(List(1,2,3,4,5), 6))
+    println(CList.drop(CList(), 0))
+    println(CList.drop(CList(), 1))
+    println(CList.drop(CList(1,2,3,4,5), 0))
+    println(CList.drop(CList(1,2,3,4,5), 1))
+    println(CList.drop(CList(1,2,3,4,5), 5))
+    println(CList.drop(CList(1,2,3,4,5), 6))
 
     println("// Exercise 3.5")
-    println(List.dropWhile(List(1,2,3,4,5), (x: Int) => x < 4))
-    println(List.dropWhile(List(1,2,3,4,5), (x: Int) => x < 6))
-    println(List.dropWhile(List(), (x: Int) => x < 6))
-    println(List.dropWhile(List(1,2,3), (x: Int) => x > 6))
+    println(CList.dropWhile(CList(1,2,3,4,5), (x: Int) => x < 4))
+    println(CList.dropWhile(CList(1,2,3,4,5), (x: Int) => x < 6))
+    println(CList.dropWhile(CList(), (x: Int) => x < 6))
+    println(CList.dropWhile(CList(1,2,3), (x: Int) => x > 6))
 
     println("// Exercise 3.6")
-    println(List.init(List(1,2,3,4,5)))
-    println(List.init(List(1)))
-    println(List.init(List()))
+    println(CList.init(CList(1,2,3,4,5)))
+    println(CList.init(CList(1)))
+    println(CList.init(CList()))
 
     println("// Exercise 3.8")
-    println(List.foldRight(List(1,2,3), Nil:List[Int])(Cons(_,_)))
+    println(CList.foldRight(CList(1,2,3), CNil:CList[Int])(CCons(_,_)))
 
     println("// Exercise 3.9")
-    println(List.length(List()))
-    println(List.length(List(1)))
-    println(List.length(List(1,2,3)))
+    println(CList.length(CList()))
+    println(CList.length(CList(1)))
+    println(CList.length(CList(1,2,3)))
 
     println("// Exercise 3.12")
-    println(List.reverse(List()))
-    println(List.reverse(List(1)))
-    println(List.reverse(List(1,2,3)))
+    println(CList.reverse(CList()))
+    println(CList.reverse(CList(1)))
+    println(CList.reverse(CList(1,2,3)))
 
     println("// Exercise 3.14")
-    println(List.append(List(1,2,3), List(4,5,6)))
+    println(CList.append(CList(1,2,3), CList(4,5,6)))
 
     println("// Exercise 3.15")
-    println(List.myLittleFlatten(List(List(5,6,7), List(8,9,0))))
+    println(CList.myLittleFlatten(CList(CList(5,6,7), CList(8,9,0))))
 
     println("// Exercise 3.16")
-    println(List.transformPlusOne(List(1,2,3,4,5)))
+    println(CList.transformPlusOne(CList(1,2,3,4,5)))
 
     println("// Exercise 3.17")
-    println(List.stringify(List(2.0, 3.0, 4.0)))
+    println(CList.stringify(CList(2.0, 3.0, 4.0)))
 
     println("// Exercise 3.18")
-    println(List.map(List(1,2,3,4))(x => x + 1))
+    println(CList.map(CList(1,2,3,4))(x => x + 1))
 
     println("// Exercise 3.19")
-    println(List.filter(List(1,2,3,4,5,6,7,8,9))(x => x % 2 == 0))
+    println(CList.filter(CList(1,2,3,4,5,6,7,8,9))(x => x % 2 == 0))
 
     println("// Exercise 3.20")
-    println(List.flatMap(List(1,2,3))(i => List(i,i)))
+    println(CList.flatMap(CList(1,2,3))(i => CList(i,i)))
 
     println("// Exercise 3.21")
-    println(List.filter(List(1,2,3,4,5,6,7,8,9))(x => x % 2 == 0))
+    println(CList.filter(CList(1,2,3,4,5,6,7,8,9))(x => x % 2 == 0))
 
     println("// Exercise 3.22")
-    println(List.addPairs(List(1,1,1), List(1,1,1)))
+    println(CList.addPairs(CList(1,1,1), CList(1,1,1)))
 
     println("// Exercise 3.23")
     def substract(a: Int, b: Int) = a-b
-    println(List.zipWith(List(1,1,1), List(1,1,1))(substract))
+    println(CList.zipWith(CList(1,1,1), CList(1,1,1))(substract))
+
+    println("// Exercise 3.24")
+    println("_begins_with: ")
+    println(CList._begins_with(
+      CList(1,2,3,4,5),
+      CList(1,2,3)
+    ))
+    println("hasSubsequence: ")
+    println(CList.hasSubsequence(
+      CList(1,2,3,4,5,6,7,8,9,0),
+      CList(7,8,9)
+    ))
   }
 }
